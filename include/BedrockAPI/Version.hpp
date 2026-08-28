e3c2eb7d610660d67ca9604784e1fbd002dbdfff
victorhilario <hilariodasilvavictor@gmail.com>
2026-08-27 22:48:56 -0300

    Bump Bedrock API to 0.2.0

--- include/BedrockAPI/Version.hpp
+++ include/BedrockAPI/Version.hpp
@@ -1,5 +1,5 @@
 #pragma once
 #define BEDROCK_API_VERSION_MAJOR 0
-#define BEDROCK_API_VERSION_MINOR 1
+#define BEDROCK_API_VERSION_MINOR 2
 #define BEDROCK_API_VERSION_PATCH 0
-namespace bedrock::api { inline constexpr const char* VersionString = "0.1.0"; }
+namespace bedrock::api { inline constexpr const char* VersionString = "0.2.0"; }
