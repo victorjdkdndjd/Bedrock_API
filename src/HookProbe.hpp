293a7ad22443b80ec14aff95c48af2ba2c11b284
victorhilario <hilariodasilvavictor@gmail.com>
2026-08-28 01:11:31 -0300

    Expose v0.3 PlayerBridge probe metrics

--- src/HookProbe.hpp
+++ src/HookProbe.hpp
@@ -7,5 +7,8 @@ namespace bedrock::api::hookprobe {
 [[nodiscard]] bool install();
 void uninstall() noexcept;
 [[nodiscard]] std::uint64_t calls() noexcept;
+[[nodiscard]] std::uint64_t clientCalls() noexcept;
+[[nodiscard]] std::uint64_t matches() noexcept;
+[[nodiscard]] std::uint64_t mismatches() noexcept;
 
 } // namespace bedrock::api::hookprobe
